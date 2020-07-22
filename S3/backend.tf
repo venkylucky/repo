7d5eb1bfc533521c033e078370d96587328acb41
Ghosh <subhajghosh@deloitte.com>
2020-07-22 20:39:06 +0530

    Adding role arn

--- S3/backend.tf
+++ S3/backend.tf
@@ -1,6 +1,6 @@
 terraform{
   backend "s3" {
-	bucket = "test"
+	bucket = "kaiburr-test"
 	key    = "statefile/S3.tfstate"
 	region = "us-east-1"
 	role_arn = "arn:aws:iam::759442462106:role/terraform-test-role"
