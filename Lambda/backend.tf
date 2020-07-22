487857bf58c677ea63a9481c0276a168f18e3237
Ghosh <subhajghosh@deloitte.com>
2020-07-22 23:27:36 +0530

    test

--- Lambda/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform{
-  backend "s3" {
-	bucket = "kaiburr-test1"
-	key    = "statefile/terraform.tfstate"
-	region = "us-east-2"
-	role_arn = "arn:aws:iam::759442462106:role/terraform-test-role"
-     }
-}
--- main.tf
+++ main.tf
@@ -5,6 +5,15 @@ provider "aws" {
 }
 }
 
+terraform{
+  backend "s3" {
+	bucket = "kaiburr-test1"
+	key    = "statefile/terraform.tfstate"
+	region = "us-east-2"
+	role_arn = "arn:aws:iam::759442462106:role/terraform-test-role"
+     }
+}
+
 module "s3" {
  source = "./S3"
 }
