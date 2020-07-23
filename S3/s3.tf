7850fa0dcd0ed5c7aa3a4a11fc7a7cc723b6d131
Ghosh <subhajghosh@deloitte.com>
2020-07-23 11:07:02 +0530

    static web hosting

--- S3/s3.tf
+++ S3/s3.tf
@@ -6,11 +6,26 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "s3bucket" {
-  bucket = "kaiburr-test2"
+  bucket = "kaiburr-test3"
   acl    = "private"
 
   tags = {
     Name        = "MyBucket"
     Environment = "Dev"
+  }
+
+  versioning {
+    enabled = true 
+  }
+
+  cors_rule {
+     allowed_headers = ["*"]
+     allowed_methods = ["PUT", "POST"]
+     allowed_origins = ["https://s3-website-test.hashicorp.com"]
+  }
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
   }
 }
