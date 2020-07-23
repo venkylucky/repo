50789e7b011b5df59712d4d5d5bb31a0cf673bed
Ghosh <subhajghosh@deloitte.com>
2020-07-23 16:59:44 +0530

    test

--- S3/s3.tf
+++ S3/s3.tf
@@ -18,14 +18,36 @@ resource "aws_s3_bucket" "s3bucket" {
     enabled = true 
   }
 
-  cors_rule {
-     allowed_headers = ["*"]
-     allowed_methods = ["PUT", "POST"]
-     allowed_origins = ["https://s3-website-test.hashicorp.com"]
-  }
-
   website {
     index_document = "index.html"
     error_document = "error.html"
   }
+}
+
+resource "aws_s3_bucket_public_access_block" "s3publicaccess" {
+  bucket = "${aws_s3_bucket.s3bucket.id}"
+
+  block_public_acls   = false
+  block_public_policy = false
+  ignore_public_acls  = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "s3bucketpolicy" {
+  bucket = "${aws_s3_bucket.s3bucket.id}"
+
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "PublicReadGetObject",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": "s3:GetObject",
+            "Resource": "arn:aws:s3:::kaiburr-test3/*"
+        }
+    ]
+}
+POLICY
 }
